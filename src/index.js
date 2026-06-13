f363a95086f6863620b73f453778c3e11ae4a977
Katie Chau <katieyourshoes@gmail.com>
2026-06-12 20:41:40 -0700

    y

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+// in the future protect this data through a separate web
+const firebaseConfig = {
+    apiKey: "AIzaSyBWMcPF7s_QvAu-0GZe_j5p9_y0TxlUeVY",
+    authDomain: "crumbz-c6eb9.firebaseapp.com",
+    projectId: "crumbz-c6eb9",
+    storageBucket: "crumbz-c6eb9.firebasestorage.app",
+    messagingSenderId: "610339390195",
+    appId: "1:610339390195:web:c157672c4e58eaa7942d86",
+    measurementId: "G-FRG3Z5110B"
+};
