94f2123ef1f4492a5251e1ec04194a7c318525dc
Katie Chau <katieyourshoes@gmail.com>
2026-06-13 22:51:41 -0700

    y

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './javascript/main.js',
+  entry: './co/javascript/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'javascript')
+    path: path.resolve(__dirname, './javascript')
   },
 };
