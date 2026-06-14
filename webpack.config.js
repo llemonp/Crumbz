db3d56ed059ad120bf9a7dad324f96cff1d8536f
Katie Chau <katieyourshoes@gmail.com>
2026-06-14 13:53:15 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,3 @@
-/*const path = require('path');
-
-module.exports = {
-  mode: 'development',
-
-  entry: './javascript/main.js',
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'javascript')
-  },
-
-  devtool: 'source-map',
-
-  resolve: {
-    extensions: ['.js'],
-    mainFields: ['module', 'main']
-  }
-};*/
-
 require('dotenv').config();
 const webpack = require('webpack');
 
@@ -52,7 +32,9 @@ module.exports = {
   // firebase file will work and API_KEY is assigned the hidden variable
   plugins: [
     new webpack.DefinePlugin({
-      API_KEY: JSON.stringify(process.env.API_KEY)
+      "process.env": {
+        API_KEY: JSON.stringify(process.env.API_KEY)
+      }
     })
   ]
 };
