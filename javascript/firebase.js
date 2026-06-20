ae8ff61f625cc9fcc6296fb80ca48a0244dbe272
Katie Chau <katieyourshoes@gmail.com>
2026-06-20 14:27:37 -0700

    y

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -1,18 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: "crumbz-c6eb9.firebaseapp.com",
-  projectId: "crumbz-c6eb9",
-  storageBucket: "crumbz-c6eb9.firebasestorage.app",
-  messagingSenderId: "610339390195",
-  appId: "1:610339390195:web:c157672c4e58eaa7942d86",
-  measurementId: "G-FRG3Z5110B"
+  authDomain: "crumbz-5edb8.firebaseapp.com",
+  projectId: "crumbz-5edb8",
+  storageBucket: "crumbz-5edb8.firebasestorage.app",
+  messagingSenderId: "620363865035",
+  appId: "1:620363865035:web:24f0c14150525ced4d744e",
+  measurementId: "G-PYGRW3F77F"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
